c6c2d4beac02a2f15b4650f8ddeade53ea4e9b03
nathantspencer <nathantspencer@gmail.com>
2016-08-22 19:31:12 -0400

    added medication name grabber

--- pdr_surfer.py
+++ pdr_surfer.py
@@ -35,12 +35,19 @@ def pdr_surf(medications):
         pea_soup = soup.findAll('a', text=re.compile('Drug Summary'))
         soup_string = str(pea_soup.pop())
         href = re.search('a href="(.*?)"', soup_string)
-        reponse = requests.get(href.group(1))
+        drug_summary_link = href.group(1)
+        print(drug_summary_link)
+        response = requests.get(drug_summary_link)
 
         # parse drug summary page for...
         soup = BeautifulSoup(response.text, 'lxml')
 
         # medication name
+        pea_soup = soup.findAll('div', { "class" : "drugSummaryLabel" }, \
+            text=re.compile(medication.title()))
+        soup_string = str(pea_soup.pop())
+        medication_name = re.search('>(.*?)<', soup_string)
+        medication_name = medication_name.group(1)
 
         # generic name
 
